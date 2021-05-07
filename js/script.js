a13b2a54f1a85ba13eac9a1440e838fdf00877c9
rygrant520 <82001861+rygrant520@users.noreply.github.com>
2021-05-06 20:17:08 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,151 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+const itemsPerPage = 9;
+const studentList = document.querySelector('.student-list');
+const linkList = document.querySelector('.link-list');
+
+
+
+
+// makes each page 9 students
+function showPage (list, page){
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   studentList.innerHTML = "";
+
+//itterates through to add li student elements to end of list
+   for(let i = 0; i < list.length; i++){
+      if(i >= startIndex && i < endIndex) {
+      
+         let student = 
+         `<li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src = ${data[i].picture.large} alt="Profile Picture">
+               <h3 >${data[i].name.first} ${data[i].name.last}</h3>
+               <span class="email">${data[i].email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date"> Joined ${data[i].registered.date}</span>
+            </div>
+         </li>`
+      
+         studentList.insertAdjacentHTML('beforeend', student);
+
+}
+   }
+      
+};
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+//creates number of page buttons based on data length
+function addPagination (list){
+   let numberOfPages = Math.ceil(list.length / itemsPerPage);
+   linkList.innerHTML = '';
+   let active = '';
+   
+//adds buttons to page
+   for(let i = 1; i <= numberOfPages; i++){
+      const pageButton = `
+      <li>
+      <button type="button"> ${i}</button>
+    </li> 
+      `
+
+   linkList.insertAdjacentHTML('beforeend',pageButton);
+   }
+   //adds active status to buttons
+      active = document.querySelector('button');
+      active.className = 'active';
+   
+   //adds an event listener to remove active status and add active status to event target
+    linkList.addEventListener('click', (e) => {
+
+      if(e.target.tagName == 'BUTTON'){
+         let activeButton = document.querySelector('.active');
+         activeButton.classList.remove('active');
+         e.target.classList.add('active');
+         showPage(list, e.target.textContent);
+
+      }
+         
+    });  
+
+}
+
+//add search bar to header
+let searchBar = `<label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>`
+
+   const header = document.querySelector('.header');
+   header.insertAdjacentHTML('beforeEnd', searchBar);
+
+  
+
+//filter students by input
+function searchFilter (list) {
+   const inputValue = document.querySelector('.student-search input').value.toLowerCase();
+   const results = [];
+
+//loop over list and push into array
+
+    for (let i = 0; i < list.length; i++) {
+        let result = list[i].name.first.toLowerCase() + ' ' + list[i].name.last.toLowerCase();
+        if (result !== 0 && result.includes(inputValue)){
+            results.push(list[i]);
+         }
+      
+ // display results of input
+showPage(results, 1);
+addPagination(results); 
+      }   
+
+  //Display a no results found message 
+   if(results.length === 0){
+   const createDiv = document.createElement('div');
+   const noResults = document.createElement('p');
+   createDiv.appendChild(noResults);
+   document.querySelector('.student-list').append(createDiv);
+   noResults.textContent = "No Results Found";
+}  };
+
+const search = document.querySelector('#search');
+const submit = document.getElementById('#search-button');
+
+submit.addEventListener('click', (e) => {
+   e.preventDefault();
+   searchFilter(search,data);
+});
+
+search.addEventListener('keyup', () => {
+   searchFilter(search,data);
+});
+
+
+// Call functions
+showPage(data, 1);
+addPagination(data);
+searchFilter(data);
